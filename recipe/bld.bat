b74c91aed1a34373713ca3c0f651c327abd0e1d4
Michael Sarahan <msarahan@gmail.com>
2017-09-11 22:42:48 -0500

    remove unnecessary site.cfg stuff and intel compiler detection patch

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,31 +1,15 @@
 if "%blas_impl%" == "mkl" (
 
-(
-echo [mkl]
-echo library_dirs = %LIBRARY_LIB%
-echo include_dirs = %LIBRARY_INC%
-echo mkl_libs = mkl_core_dll, mkl_intel_lp64_dll, mkl_intel_thread_dll
-echo lapack_libs = mkl_lapack95_lp64
-) > site.cfg
-
-) else (
-
-(
-echo [DEFAULT]
-echo library_dirs = %LIBRARY_LIB%
-echo include_dirs = %LIBRARY_INC%
-echo.
-echo [atlas]
-echo atlas_libs = openblas
-echo libraries = openblas
-echo.
-echo [openblas]
-echo libraries = openblas
-echo library_dirs = %LIBRARY_LIB%
-echo include_dirs = %LIBRARY_INC%
-) > site.cfg
+:: echo newlines
+echo/ >> numpy/__init__.py
+echo/ >> numpy/__init__.py
+echo __mkl_version__ = "%mkl%" >> numpy/__init__.py
+echo/ >> numpy/__init__.py
 
 )
+
+:: site.cfg comes from blas devel package (e.g. mkl-devel)
+COPY %PREFIX%\site.cfg site.cfg
 
 python setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
