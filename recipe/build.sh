e524103db67865261098ea705a143b23e8e075cc
Michael Sarahan <msarahan@gmail.com>
2017-09-12 22:05:53 -0500

    remove site.cfg stuff - copy one provided by blas impl (only done in mkl so far)

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,36 +4,12 @@ cp $RECIPE_DIR/test_fft.py numpy/fft/tests
 
 if [ "$blas_impl" = "mkl" ]; then
 
-cat > site.cfg <<EOF
-[mkl]
-library_dirs = $PREFIX/lib
-include_dirs = $PREFIX/include
-mkl_libs = mkl_rt
-lapack_libs =
-
-EOF
-
 printf "\n\n__mkl_version__ = \"$mkl\"\n" >> numpy/__init__.py
 
-else # Non-MKL
-
-cat > site.cfg <<EOF
-[DEFAULT]
-library_dirs = $PREFIX/lib
-include_dirs = $PREFIX/include
-
-[atlas]
-atlas_libs = openblas
-libraries = openblas
-
-[openblas]
-libraries = openblas
-library_dirs = $PREFIX/lib
-include_dirs = $PREFIX/include
-
-EOF
-
 fi
+
+# site.cfg comes from blas devel package (e.g. mkl-devel)
+cp $PREFIX/site.cfg site.cfg
 
 if [[ ${HOST} =~ .*darwin.* ]]; then
     export LDFLAGS="${LDFLAGS} -undefined dynamic_lookup"
