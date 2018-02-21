c350c7dcdf29966c53d48312173ce8f8da3348ee
Filipe Fernandes <ocefpaf@gmail.com>
2018-02-21 09:29:09 -0300

    remove patches

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,16 +8,5 @@ printf "\n\n__mkl_version__ = \"$mkl\"\n" >> numpy/__init__.py
 
 fi
 
-<<<<<<< HEAD
-# site.cfg comes from blas devel package (e.g. mkl-devel)
-cp $PREFIX/site.cfg site.cfg
 
-if [[ ${HOST} =~ .*darwin.* ]]; then
-    export LDFLAGS="${LDFLAGS} -undefined dynamic_lookup"
-fi
-
-# $PYTHON setup.py config
-# $PYTHON setup.py build
-=======
->>>>>>> all pip
 $PYTHON -m pip install --no-deps --ignore-installed .
