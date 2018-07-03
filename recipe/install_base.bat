a6389a6191fe5c310430fb382f45e7961a2c6598
Michael Sarahan <msarahan@gmail.com>
2018-07-03 16:28:56 +0100

    revert site.cfg changes (should come from blas package)

--- recipe/install_base.bat
+++ recipe/install_base.bat
@@ -1,31 +1,6 @@
-if "%blas_impl%" == "mkl" (
+:: site.cfg should not be defined here.  It is provided by blas devel packages (either mkl-devel or openblas-devel)
 
-(
-echo [mkl]
-echo library_dirs = %LIBRARY_LIB%
-echo include_dirs = %LIBRARY_INC%
-echo mkl_libs = mkl_core_dll, mkl_intel_lp64_dll, mkl_intel_thread_dll
-echo lapack_libs = mkl_lapack95_lp64
-) > site.cfg
-
-) else (
-
-(
-echo [DEFAULT]
-echo library_dirs = %LIBRARY_LIB%
-echo include_dirs = %LIBRARY_INC%
-echo.
-echo [atlas]
-echo atlas_libs = openblas
-echo libraries = openblas
-echo.
-echo [openblas]
-echo libraries = openblas
-echo library_dirs = %LIBRARY_LIB%
-echo include_dirs = %LIBRARY_INC%
-) > site.cfg
-
-)
+COPY %PREFIX%\site.cfg site.cfg
 
 python setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
--- recipe/install_base.sh
+++ recipe/install_base.sh
@@ -1,39 +1,9 @@
 #!/bin/bash
 
+cp $PREFIX/site.cfg site.cfg
 cp $RECIPE_DIR/test_fft.py numpy/fft/tests
 
-if [ "$blas_impl" = "mkl" ]; then
-
-cat > site.cfg <<EOF
-[mkl]
-library_dirs = $PREFIX/lib
-include_dirs = $PREFIX/include
-mkl_libs = mkl_rt
-lapack_libs =
-
-EOF
-
-printf "\n\n__mkl_version__ = \"$mkl\"\n" >> numpy/__init__.py
-
-else # Non-MKL
-
-cat > site.cfg <<EOF
-[DEFAULT]
-library_dirs = $PREFIX/lib
-include_dirs = $PREFIX/include
-
-[atlas]
-atlas_libs = openblas
-libraries = openblas
-
-[openblas]
-libraries = openblas
-library_dirs = $PREFIX/lib
-include_dirs = $PREFIX/include
-
-EOF
-
-fi
+# site.cfg should not be defined here.  It is provided by blas devel packages (either mkl-devel or openblas-devel)
 
 if [[ $(uname) == 'Darwin' ]]; then
     export LDFLAGS="$LDFLAGS -undefined dynamic_lookup"
