9b7fde661d04cd6f0f54deeb7708a1451b640311
Michael Sarahan <msarahan@gmail.com>
2018-07-25 15:59:02 -0500

    rework patches for 1.15 update

--- recipe/numpy_test.py
+++ recipe/numpy_test.py
@@ -3,11 +3,7 @@ import sys
 import numpy
 
 import numpy.core.multiarray
-import numpy.core.multiarray_tests
 import numpy.core.numeric
-import numpy.core.operand_flag_tests
-import numpy.core.struct_ufunc_test
-import numpy.core.test_rational
 import numpy.core.umath
 import numpy.core.umath_tests
 import numpy.fft.fftpack_lite
@@ -31,7 +27,7 @@ elif sys.platform.startswith('linux'):
     os.environ['LDFLAGS'] = ' '.join((os.getenv('LDFLAGS', ''), '-shared'))
     os.environ['FFLAGS'] = ' '.join((os.getenv('FFLAGS', ''), '-Wl,-shared'))
 result = numpy.test()
-if sys.version_info[0:2] == (3, 7) and not result.wasSuccessful():
+if sys.version_info[0:2] == (3, 7) and not result:
     print("WARNING :: Ignoring numpy test failure on Python 3.7")
     sys.exit(0)
-sys.exit(not result.wasSuccessful())
+sys.exit(not result)
